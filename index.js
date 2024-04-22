bff65add0f46bdf39d9aa06f21fcde190e1f2638
jyfhmr <80013819+jyfhmr@users.noreply.github.com>
2024-04-21 20:25:39 -0400

    Corregí el bug

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 // Move the mouse across the screen as a sine wave.
 var robot = require("@todesktop/robotjs-prebuild");
 const { exec } = require("child_process");
-const XLSX = require("xlsx");
-const workbook = XLSX.readFile("./personas.xlsx");
-const sheet_name_list = workbook.SheetNames;
-const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-
-const result = [];
-
 let asdasd="a"
-
-
-data.forEach((row) => {
-  const whatsapp = row["Telefono"];
-  const copy = row["copy antispan"];
-  
-  
-  if (whatsapp && copy) {
-    result.push({
-      whatsapp: whatsapp,
-      copy: copy,
-    });
-  }
-});
-
-console.log("resultado", result);
 
 function delay(ms) {
   const date = Date.now();
@@ -56,11 +33,9 @@ function openWhatsAppApp(phone) {
   });
 }
 
-
 let phones2 = [
   584123091835, 584169372345, 584245036249, 584122103424,584123091835, 584169372345, 584245036249, 584122103424,584123091835, 584169372345, 584245036249, 584122103424,584123091835, 584169372345, 584245036249, 584122103424,584123091835, 584169372345, 584245036249, 584122103424,
 ];
-
 
 let contador = 0;
 
